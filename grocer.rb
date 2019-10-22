ad717c85c2e77ea6292a409ced09966519b46c00
Guligena Aierken <guligena_aierken@163.com>
2019-10-22 01:08:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,6 +23,7 @@
 def apply_coupons(cart, coupons)
     coupons.each do |coupon|
       item=coupon[:item]
+      coupon_item="#{item} W/COUPON"
       if cart[item]
         if cart[item][:count]>=coupon[:num] && !cart.has_key?("#{item} W/COUPON")
            cart["#{item} W/COUPON"]={price: coupon[:cost]/coupon[:num], clearance: cart[item][:clearance],count:coupon[:num]}
